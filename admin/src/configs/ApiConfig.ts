21a95f117c6f90bdfeca46efafc4e153d37f47c7
nitinsharma450 <nitinsharma1059@gmail.com>
2025-08-24 17:08:04 +0530

    Updated admin code with new changes

--- admin/src/configs/ApiConfig.ts
+++ admin/src/configs/ApiConfig.ts
@@ -1,5 +1,5 @@
 export const ApiConfig={
-   EndPoint:'http://localhost:9999/api/admin/',
+   EndPoint:'https://shopcraft-backend-3wl9h9cy6-nitinsharma1059-1842s-projects.vercel.app/api/admin/',
    Token : 'admin_auth_token',
    AdminId : 'admin_auth_id'
 } 
