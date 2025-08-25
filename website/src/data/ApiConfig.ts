6dabe8d86b9dccc04d00a37e1d3985bdb29d0d91
nitinsharma450 <nitinsharma1059@gmail.com>
2025-08-25 15:33:57 +0530

    updated on website code

--- website/src/data/ApiConfig.ts
+++ website/src/data/ApiConfig.ts
@@ -1,4 +1,4 @@
 export const ApiConfig = {
-    Endpoint : ' https://shopcraft-backend-gitx06ssp-nitinsharma1059-1842s-projects.vercel.app/api/website/',
+    Endpoint : 'https://shopcraft-backend-gitx06ssp-nitinsharma1059-1842s-projects.vercel.app/api/website/',
     CartLocalStorageKey : 'cart_items'
 }
